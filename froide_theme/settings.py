5ceaed883a0dcbbfc1975de9ad8a722a0f95cd0a
Mart van de Ven <m@type.hk>
2014-03-08 20:54:58 +0800

    Replaces froide dependency with ODHK fork

--- froide_theme/settings.py
+++ froide_theme/settings.py
@@ -5,8 +5,8 @@ from froide.settings import Base, ThemeBase, HerokuPostmark, HerokuPostmarkS3  #
 class CustomThemeBase(ThemeBase):
     FROIDE_THEME = 'froide_theme.theme'
 
-    SITE_NAME = "My Froide"
-    SITE_EMAIL = "info@example.com"
+    SITE_NAME = "OpenGov.HK"
+    SITE_EMAIL = "foi@opendatahk.com"
     SITE_URL = 'http://localhost:8000'
 
     SECRET_URLS = {
